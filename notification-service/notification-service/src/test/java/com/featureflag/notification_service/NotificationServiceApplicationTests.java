7c602d932e9d56917504c47819810d6dfc127f77
Manish Jangid <manishjangid1499@gmail.com>
2026-08-25 17:50:46 +0530

    test(notification-service): add hermetic context test profile

--- notification-service/notification-service/src/test/java/com/featureflag/notification_service/NotificationServiceApplicationTests.java
+++ notification-service/notification-service/src/test/java/com/featureflag/notification_service/NotificationServiceApplicationTests.java
@@ -1,8 +1,13 @@
 package com.featureflag.notification_service;
 
+import com.featureflag.notification_service.config.KafkaListenerTestConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
+@Import(KafkaListenerTestConfiguration.class)
 @SpringBootTest
 class NotificationServiceApplicationTests {
 
